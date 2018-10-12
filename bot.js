73cfda2364b951e52735d75f3dcb97ef0ab341a7
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-10-12 05:40:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -211,30 +211,30 @@ client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
 
-  if (message.content.startsWith(adminprefix + 'ply')) {
+  if (message.content.startsWith(adminprefix + 'vip play')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
   } else
-     if (message.content === (adminprefix + "leave")) {
+     if (message.content === (adminprefix + "lev")) {
     message.guild.leave();
   } else
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
   } else
-  if (message.content.startsWith(adminprefix + 'ls')) {
+  if (message.content.startsWith(adminprefix + 'vip li')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
   } else
-  if (message.content.startsWith(adminprefix + 'st')) {
+  if (message.content.startsWith(adminprefix + 'vip strimng')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**✅ تم تغيير الحالة بنجاح **`)
   }
-  if (message.content.startsWith(adminprefix + 'setname')) {
+  if (message.content.startsWith(adminprefix + 'vip name')) {
   client.user.setUsername(argresult).then
       message.channel.send(`**✅ تم تغير الاسم بنجاح ${argresult}** `)
 } else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
+if (message.content.startsWith(adminprefix + 'vip avatar')) {
   client.user.setAvatar(argresult);
     message.channel.send(`**✅ تم تغير الصور بنجاح ${argresult}** `);
 }
