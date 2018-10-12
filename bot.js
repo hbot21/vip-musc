6c99bcff0bcb8103d82ff12064a3a50e32e6021b
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-10-12 06:00:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -211,7 +211,7 @@ client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
 
-  if (message.content.startsWith(adminprefix + 'play')) {
+  if (message.content.startsWith(adminprefix + 'gam')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
   } else
