4144e5c5579e148f7dc02f2ff1e71da51f484212
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-10-18 21:01:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -240,7 +240,16 @@ if (message.content.startsWith(adminprefix + 'avatar')) {
 }
 });
 
-
+client.on('message' , message => {
+  if(message.author.bot) return;
+  if(message.content.startsWith(prefix + "ping")) {
+ message.channel.send('pong').then((msg) => {
+var PinG = `${Date.now() - msg.createdTimestamp}`
+var ApL = `${Math.round(client.ping)}`
+      msg.edit(`\`\`\`javascript\nTime taken: ${PinG} ms.\nDiscord API: ${ApL} ms.\`\`\``);
+ })
+  }  
+ });
 
  
  client.login(process.env.BOT_TOKEN);
