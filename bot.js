121e108b5bb38b4a35efa01bc12c3863b3937ffd
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-10-18 21:13:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -251,5 +251,27 @@ var ApL = `${Math.round(client.ping)}`
   }  
  });
 
+client.on('message', message => {
+    if (message.content.startsWith("$bot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO Arap Bot`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+			      .addField('``My Prefix``' , `[ - ]` , true)
+			      .addField('``My Language``' , `[ Java Script ]` , true)
+			      .setFooter('By | ,~ HAMO♚ , .')
+    })
+}
+});
+
  
  client.login(process.env.BOT_TOKEN);
