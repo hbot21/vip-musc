4d4bc9f5b285815cfeb1594b7a9c9433d343e2f8
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-10-27 22:09:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -206,7 +206,7 @@ function isYoutube(str) {
 
 
 const devs = ["466425075487342615"];
-const adminprefix = ["$v"];
+const adminprefix = ["h"];
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
@@ -1360,5 +1360,18 @@ client.on("message", message => {
       }
   });
 
+
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('$bcall')){
+ if (message.author.id !== '466425075487342615') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '466425075487342615') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
  client.login(process.env.BOT_TOKEN);
