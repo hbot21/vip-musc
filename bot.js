5a897f12b574239507a32d3b7bb415ae6b0af6bf
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-10-28 18:27:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -275,5 +275,29 @@ message.author.send(`**مدة الرابط : يـوم
 
 
 
+client.on('message', message => {
+    if (message.content.startsWith(prefix + 'مسح')) {
+      if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(`ماعندك هذا البرمشن[*MANAGE_MESSAGES*] `).catch(console.error);
+  message.delete()
+  if(!message.channel.guild) return;
+  let args = message.content.split(" ").slice(1);
+  
+  const messagecount = parseInt(args.join(' '));
+  
+  message.channel.fetchMessages({
+  
+  limit: messagecount
+  
+  }).then(messages => message.channel.bulkDelete(messages));
+  message.channel.sendMessage("", {embed: {
+    title: "**``تــم مسح الشات ``**",
+    color: 0x06DF00,
+    footer: {
+    
+    }
+    }}).then(msg => {msg.delete(3000)});
+  };
+  
+  });
 
 client.login(process.env.BOT_TOKEN);
