3d08042bbd1cb24231a01a1f831105cf998703fa
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-10-28 12:54:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1342,23 +1342,7 @@ client.on("message", message => {
   };
  })
 
-client.on("message", message => {
-              
-          if(!message.channel.guild) return;
-   if(message.author.bot) return;
-      if(message.content === prefix + "avatar server"){ 
-          const embed = new Discord.RichEmbed()
-  
-      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
-  .setAuthor(message.author.username, message.guild.iconrURL)
-    .setColor(0x164fe3)
-    .setImage(message.guild.iconURL)
-    .setURL(message.guild.iconrURL)
-                    .setTimestamp()
 
-   message.channel.send({embed});
-      }
-  });
 
 client.on('ready', () => {
    console.log(`----------------`);
